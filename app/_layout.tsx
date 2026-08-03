00aec629af5cfc849c334ed0aee112b70116c6c1
Karen Navarro <karennavarrofigueroa@protonmail.com>
2026-08-03 10:53:17 -0400

    ok

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,5 @@
 import { Stack } from 'expo-router';
 
-<Stack.Screen name="emergency-active" />
-
 export default function RootLayout() {
   return <Stack screenOptions={{ headerShown: false }} />;
 }
